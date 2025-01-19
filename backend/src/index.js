7f88cff060a33cd2937fb88563c33181f717fc0b
davidvchang <dvalenzuelachang@gmail.com>
2025-01-19 12:08:58 -0700

    Se cambio el archivo index.js para poder subir el backend a vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,14 @@ import {openConnection} from "./bd.js"
 
 openConnection();
 
-async function main() {
-    await app.listen(app.get("port"))
-    console.log(`Server on port: ${app.get("port")}`)
-}
+//SE QUITA ESTO PARA PODER SUBIRLO A VERCEL
 
-main()
+// async function main() {
+//     await app.listen(app.get("port"))
+//     console.log(`Server on port: ${app.get("port")}`)
+// }
+
+// main()
+
+//SE AGREGA ESTO PARA SUBIRLO A VERCEL
+export default app;
