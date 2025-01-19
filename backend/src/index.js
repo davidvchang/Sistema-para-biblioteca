e78b63844214ac8c1547e52914bbc47ee00c16dd
davidvchang <dvalenzuelachang@gmail.com>
2025-01-19 12:50:29 -0700

    se actualizó el archivo index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,9 @@ import {openConnection} from "./bd.js"
 
 openConnection();
 
-//SE QUITA ESTO PARA PODER SUBIRLO A VERCEL
+async function main() {
+    await app.listen(app.get("port"))
+    console.log(`Server on port: ${app.get("port")}`)
+}
 
-// async function main() {
-//     await app.listen(app.get("port"))
-//     console.log(`Server on port: ${app.get("port")}`)
-// }
-
-// main()
-
-//SE AGREGA ESTO PARA SUBIRLO A VERCEL
-export default app;
+main()
